e5d092dd55f3301f5e7f63ed685d0ca19d624a0c
FUTURE TALK <winstonmr098@gmail.com>
2025-05-19 22:10:38 +0300

    Update auto_push.bat

--- auto_push.bat
+++ auto_push.bat
@@ -1,4 +1,5 @@
-cd /d "G:\Мой диск\Сайтт"
-git add .
-git commit -m "Автообновление %date% %time%"
-git push origin main
+   cd /d "G:\Мой диск\Сайтт"
+   gdown --folder https://drive.google.com/drive/folders/1olWJZN4-jsQy12hqQ9GIGpuuIcnTPk6R
+   git add .
+   git commit -m "Автообновление %date% %time%"
+   git push origin main
