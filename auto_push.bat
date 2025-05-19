8b9a2f37886f86d4938babb50ce4a59df86c63b5
FUTURE TALK <winstonmr098@gmail.com>
2025-05-19 22:20:02 +0300

    Delete auto_push.bat

--- auto_push.bat
+++
@@ -1,8 +0,0 @@
- cd /d "G:\Мой диск\Сайтт"
-del /Q *.html
-del /Q *.js
-del /Q *.css
-gdown --folder https://drive.google.com/drive/folders/1olWJZN4-jsQy12hqQ9GIGpuuIcnTPk6R
-git add .
-git commit -m "Автообновление %date% %time%"
-git push origin main
